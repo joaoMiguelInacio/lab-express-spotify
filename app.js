faa5a3c34fe74a66d16550b9f2d8415d675a6428
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-05-04 19:44:53 +0100

    Lab solved

--- app.js
+++ app.js
@@ -25,5 +25,46 @@ const spotifyApi = new SpotifyWebApi({
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
     
 // Our routes go here:
+app.get('/', (request, response) => {
+  try {  
+    response.render('index');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/artist-search', async (request, response) => {
+  try {
+    const data = await spotifyApi.searchArtists(request.query.artist);
+    const artistsData = data.body.artists.items;
+    const searchedArtist = request.query.artist;
+    response.render('artist-search', {artistsData, searchedArtist});
+  } catch (err) {
+    console.log('The error while searching artists occurred: ', err);
+  }
+});
+
+app.get('/albums/:id', async (request, response) => {
+  try {
+    const data = await spotifyApi.getArtistAlbums(request.params.id);
+    const albums = data.body.items;
+    const author = albums[0].artists[0].name;
+    response.render('albums-collection', {albums, author});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/tracks/:id', async (request, response) => {
+  try {
+    const data = await spotifyApi.getAlbumTracks(request.params.id);
+    const tracks = data.body.items;
+    const data1 = await spotifyApi.getAlbum(request.params.id);
+    const albumName = data1.body.name;
+    response.render('tracks-collection', {tracks, albumName});
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
